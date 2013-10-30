ad578768c08114c5562513075006a1e9976d2794
Andre <andre.penha.si@gmail.com>
2013-10-29 22:11:52 -0200

    IMCaula

---
+++ js/controllers/Validar.js
@@ -0,0 +1,21 @@
+function numeros(ie, ff) {
+    if (ie) {
+        tecla = ie;
+    } else {
+        tecla = ff;
+    }
+ 
+    /**
+    * 13 = [ENTER]
+    * 8  = [BackSpace]
+    * 9  = [TAB]
+    * 46 = [Delete]
+    * 48 a 57 = São os números
+    */
+    if ((tecla >= 48 && tecla <= 57) || (tecla == 8) || (tecla == 13) || (tecla == 9) || (tecla == 46)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
